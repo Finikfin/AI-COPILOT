7732305b46501c0e034b51ea595262f9ccab8478
Finikfin <finikgrib@gmail.com>
2026-03-14 11:17:19 +0300

    add pipeline model

--- backend/app/core/database/models/__init__.py
+++ backend/app/core/database/models/__init__.py
@@ -1,5 +1,6 @@
 from app.core.database.models.base import Base
 from app.core.database.models.user import User, UserRole
 from app.core.database.models.action import Action, HttpMethod
+from app.core.database.models.pipeline import Pipeline, PipelineStatus
 
-__all__ = ["Base", "User", "UserRole", "Action", "HttpMethod"]
+__all__ = ["Base", "User", "UserRole", "Action", "HttpMethod", "Pipeline", "PipelineStatus"]
